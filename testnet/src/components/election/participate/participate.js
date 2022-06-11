0635de7ee1d55f8c8ea99350d519a4bd8143cec9
Aspirin 💆🏽 <samuellyworld@gmail.com>
2022-06-12 00:38:23 +0100

    building election page --participate 💠

---
+++ testnet/src/components/election/participate/participate.js
@@ -0,0 +1,16 @@
+import React from "react";
+import './participate.scss';
+
+const ElectionCard = ()=> {
+    return (
+      <div className="election__card">
+        <h1> Lorem Ipsum</h1>
+         <p> jwwjdowjwdjwjdjdwjwjo hh2hwshhbbsxh
+
+         </p>
+         <button> VOTE</button>
+      </div>
+    );
+}
+
+export default ElectionCard;
