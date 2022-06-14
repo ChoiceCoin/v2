a027ec9c35cacc8d79f416d7df1c163173561b3c
B Haney <43055154+Bhaney44@users.noreply.github.com>
2022-06-14 08:37:57 -0700

    Update VotePage.js

--- testnet/src/pages/VotePage/VotePage.js
+++ testnet/src/pages/VotePage/VotePage.js
@@ -12,14 +12,14 @@ const VotingPage = () => {
          <div className="voting__card_header">
              <div className="voting__card">
                <h1>Vote  </h1>
-               <p> Vote on existing issues.  </p>
+               <p>  Vote on existing issues.  </p>
                <Link to='/participate'>
                  <button> Vote </button>
                </Link>
              </div>
              <div className="voting__card">
              <h1>Propose  </h1>
-               <p> Propose a new issue for decentralized governance. </p>
+               <p>  Propose a new issue for decentralized governance. </p>
                <Link to='/propose'>
                 <button> Propose</button>
                </Link>
