a35396f561878c5933ec87ca29fb95d27526b756
Aspirin 💆🏽 <samuellyworld@gmail.com>
2022-06-15 23:20:27 +0100

    fixing and debugging 📲

--- testnet/src/pages/VotePage/VotePage.js
+++ testnet/src/pages/VotePage/VotePage.js
@@ -6,15 +6,9 @@ const VotingPage = ({darkTheme}) => {
    return (
        <div className="voting__page">
          <div className="voting__header" 
-          //  style={{
-          //   backgroundColor: darkTheme? "#161616" : "white",
-          //   opacity: "0.8",
-          //   backgroundImage: "radial-gradient(#b0adad 1.1px, #161616 1.1px)",
-          //   // background-size: 22px 22px;
-          //  }}
          >
              <h1>Vote and Propose Issues </h1>
-             <p> Vote on existing issues or propose a new issue for decentralized governance.</p>
+             <p> Vote on existing issues or propose a new issue for decentralized governance. Kindly Connect Your Wallet Before Proceeding.</p>
          </div> 
          <div className="voting__card_header">
              <div className="voting__card">
