56da7b4c80a691b9654cd1577352eab281928916
Aspirin 💆🏽 <samuellyworld@gmail.com>
2022-06-15 01:52:53 +0100

    functionalities --proposal page initial ➫

--- testnet/src/components/propose/propose.js
+++ testnet/src/components/propose/propose.js
@@ -1,16 +1,75 @@
 import React from "react";
+import { useDispatch } from "react-redux";
+import algosdk from "algosdk";
 import './propose.scss';
 
 const Propose = () => {
+  const dispatch = useDispatch();
+  const isThereAddress = localStorage.getItem("address");
+  const algod_token = {
+    "X-API-Key": "z6H94GE3sI8w100S7MyY92YMK5WIPAmD6YksRDsC"
+  }
+  const algod_address = "https://testnet-algorand.api.purestake.io/ps2";
+  const headers = "";
+
+  const algodClient = new algosdk.Algodv2(algod_token, algod_address, headers);
+  const walletType = localStorage.getItem("wallet-type");
+  const rewardsAddress = 'ZW4E323O6W3JTTVCDDHIF6EY75HSU56H7AGD3UZI54XCQOMNRCWRTYP5PQ'
 
     //disable past dates
-    const disablePastDate = () => {
-        const today = new Date();
-        const dd = String(today.getDate() + 1).padStart(2, "0");
-        const mm = String(today.getMonth() + 1).padStart(2, "0"); //January is 0!
-        const yyyy = today.getFullYear();
-        return yyyy + "-" + mm + "-" + dd;
-    };
+    // const disablePastDate = () => {
+    //     const today = new Date();
+    //     const dd = String(today.getDate() + 1).padStart(2, "0");
+    //     const mm = String(today.getMonth() + 1).padStart(2, "0"); //January is 0!
+    //     const yyyy = today.getFullYear();
+    //     return yyyy + "-" + mm + "-" + dd;
+    // };
+
+    const createProposal = () => {
+
+      if(!isThereAddress) {
+        dispatch({
+          type: "alert_modal",
+          alertContent: "Kindly Connect Wallet To Make Payment.",
+        });
+        return;
+    } else if(!(document.getElementById('governance_name').value)) {
+      dispatch({
+        type: "alert_modal",
+        alertContent: "You didn't enter governance name.",
+      });
+      return;
+    }
+      
+     else if(!(document.getElementById('rewards').value)) {
+        dispatch({
+          type: "alert_modal",
+          alertContent: "Enter Choice Rewards for Governance.",
+        });
+        return;
+      } else if (!(document.getElementById('issue').value)) {
+        dispatch({
+          type: "alert_modal",
+          alertContent: "Voting issue for governance not found.",
+        });
+        return;
+      } else if (!(document.getElementById('option1').value)) {
+        dispatch({
+          type: "alert_modal",
+          alertContent: "Enter what option 1 should be ?",
+        });
+        return;
+      } else if (!(document.getElementById('option2').value)) {
+        dispatch({
+          type: "alert_modal",
+          alertContent: "Enter what option 2 should be ?",
+        });
+        return;
+      }
+
+    }
+
+
     return (
        <div className="propose">
            <div className="create_elt">
@@ -27,18 +86,27 @@ const Propose = () => {
 
           <div className="v_inp_cov inpCont_cand">
             <p className="inp_tit">Governance name</p>
-            <input
+            <input id="governance_name"
               type="text"
             />
             <p className="ensure_txt">
               A short header for your proposal
             </p>
           </div>
-
+          <div className="v_inp_cov inpCont_cand">
+            <p className="inp_tit">Rewards</p>
+            <input
+              type="text" id="rewards"
+            />
+            <p className="ensure_txt">
+              How much in $Choice Token are you giving to voters
+            </p>
+          </div>
           <div className="v_inp_cov inpCont_cand">
             <p className="inp_tit">Voting issue</p>
             <input
               type="text"
+              id="issue"
             />
             <p className="ensure_txt">
               A short description of what the issue is about
@@ -48,6 +116,7 @@ const Propose = () => {
             <p className="inp_tit">Option 1</p>
             <input
               type="text"
+              id="option1"
             />
             <p className="ensure_txt">
               What option 1 should be
@@ -58,13 +127,16 @@ const Propose = () => {
             <p className="inp_tit">Option 2</p>
             <input
               type="text"
+              id="option2"
             />
             <p className="ensure_txt">
               What option 2 should be
             </p>
           </div>
 
-          <div className="v_inp_cov inpCont_cand">
+          
+
+          {/* <div className="v_inp_cov inpCont_cand">
             <p className="inp_tit">Governance Date</p>
             <input
               type="date"
@@ -73,14 +145,14 @@ const Propose = () => {
             <p className="ensure_txt">
               Pick a date for election
             </p>
-          </div>
+          </div> */}
           
           
 
           <br />
 
           <div className="crt_butt">
-            <button>Create Proposal</button>
+            <button onClick={createProposal}>Create Proposal</button>
             <p className="safety">
               <span>Safety disclaimer :</span> We never store your data.
               Everything is encrypted.
