d8f363e280fe4b30b740ad244fa43f3a21acca1d
Aspirin 💆🏽 <samuellyworld@gmail.com>
2022-07-06 17:20:59 +0100

    removing DS_Store 🎈

--- MVP/src/App.js
+++ MVP/src/App.js
@@ -1,3 +1,7 @@
+// Copyright Fortior Blockchain 2022
+// 17 U.S.C §§ 101-1511
+
+//Importing relevant dependencies
 import { Suspense } from "react";
 import MainPage from "./pages/MainPage";
 import stores from "./store/stores";
@@ -7,11 +11,14 @@ import { Provider as ReduxProvider } from "react-redux";
 import { BrowserRouter as Router } from "react-router-dom";
 import { QueryClient, QueryClientProvider } from "react-query";
 
-
+// Suspense beforen mounting UI
 const renderLoader = () => <p></p>;
 
+// JSX Component APP
 const App = () => {
   const queryClient = new QueryClient();
+
+  // HTML Building block -- JSX
   return (
     <Suspense fallback={renderLoader()}>
       <ReduxProvider store={stores}>
--- MVP/src/index.js
+++ MVP/src/index.js
@@ -1,7 +1,12 @@
+// Copyright Fortior Blockchain 2022
+// 17 U.S.C §§ 101-1511
+
+// Importing relevant files and dependencies
 import React from "react";
 import ReactDOM from "react-dom";
 import "./styles/index.css";
 import App from "./App";
+
 
 ReactDOM.render(
   <React.StrictMode>
