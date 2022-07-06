b6fa392e080690563d3b82ab2f4e7e34926cec37
Samuel Tosin <Samuellyworld@gmail.com>
2022-07-07 00:16:53 +0100

    Update reducers.js

--- MVP/src/store/reducers.js
+++ MVP/src/store/reducers.js
@@ -21,39 +21,30 @@ const status = (
 ) => {
   //action before dispatching!
   switch (action.type) {
-    
     case "getBalance" : 
       return {...state, balance : action.balance}
-
     case "setAlgoAddress" : 
       localStorage.setItem("address", `${action?.addr}`);
       return { ...state, addressNum: action.addressIndex };
-
      case "getEachElectionNumber" :
        return  {
          ...state, eachElectionNumber: action.electionIndex
        };
-
       case "getAllElection" : 
        return {...state, allElection : action.allElection}
-
     case "alert_modal":
       return {
         ...state,
         alertModal: { openModal: true, modalContent: action.alertContent },
       };
-
     case "close_modal":
       return { ...state, alertModal: { openModal: false, modalContent: "" } };
-
     case "confirm_wallet":
       return {...state, confirmWallet : { openWallet : true, walletContent: action.alertContent }} ; 
-
     case "close_wallet" :
       return {...state, confirmWallet : {openWallet : false, walletContent : ""} };
     default:
       return state;
   }
 };
-
 export default combineReducers({ status });
