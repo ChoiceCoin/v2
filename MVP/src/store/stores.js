5d606faa99b632a71e801c0bd94f75736257dcb7
Aspirin 💆🏽 <samuellyworld@gmail.com>
2022-07-06 16:59:22 +0100

    adding simplified store folder codes 📃

--- MVP/src/store/stores.js
+++ MVP/src/store/stores.js
@@ -1,7 +1,13 @@
+// Copyright Fortior Blockchain 2022
+// 17 U.S.C §§ 101-1511
+
+// import relevant dependencies
 import { createStore } from "redux";
 import rootReducer from "./reducers";
 
+// initial state
 let initialState = {};
 
+// creating redux store
 const store = createStore(rootReducer, initialState);
 export default store;
