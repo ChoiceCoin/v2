842a91fa03e234d4d12e7fb21a6996e65c8de391
B Haney <43055154+Bhaney44@users.noreply.github.com>
2022-07-08 10:55:44 -0700

    Add files via upload

---
+++ MVP/src/pages/CompliancePage.js
@@ -0,0 +1,18 @@
+// Copyright Fortior Blockchain 2022
+// 17 U.S.C §§ 101-1511
+
+// import relevant dependencies
+import React from "react";
+import { Link } from "react-router-dom";
+
+//JSX Component
+const CompliancePage = () => {
+   return (
+       <div className="compliance__page">
+         <div className="compliance__header" >
+             <h1>Choice Coin Compliance</h1>
+             <p>Choice Coin Compliance uses an informatics based approach to automating compliance for digital assets. Choice Coin Compliance will launch by Q4 2022.</p>
+   )
+}
+
+export default CompliancePage;
---
+++ MVP/src/pages/ConverterPage.js
@@ -0,0 +1,18 @@
+// Copyright Fortior Blockchain 2022
+// 17 U.S.C §§ 101-1511
+
+// import relevant dependencies
+import React from "react";
+import { Link } from "react-router-dom";
+
+//JSX Component
+const ConverterPage = () => {
+   return (
+       <div className="converter__page">
+         <div className="converter__header" >
+             <h1> Choice Coin Converter </h1>
+             <p> Choice Coin Converter provides an interchain mechanism for cross chain conversion. The Choice Coin Converter will launch by Q4 2022. </p>
+   )
+}
+
+export default ConverterPage;
